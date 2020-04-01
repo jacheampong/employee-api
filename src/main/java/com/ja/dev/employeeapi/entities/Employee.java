b716a019f929d298c431cc052788ecca2eb5f894
jacheampong <jacheampong@gmail.com>
2020-03-31 22:45:25 -0400

    #4: Expose the ID field/value

--- src/main/java/com/ja/dev/employeeapi/entities/Employee.java
+++ src/main/java/com/ja/dev/employeeapi/entities/Employee.java
@@ -17,6 +17,14 @@ public class Employee {
 	public Long getId() {
 		return id;
 	}
+	
+	/**
+	 * getResourceId - used to expose field Id
+	 * @return
+	 */
+	public Long getResourceId() {
+		return id;
+	}
 
 	public void setId(Long id) {
 		this.id = id;
